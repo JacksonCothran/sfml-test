feb87f98a6194c7e7fc879ef4caab879b30c3fa5
Jackson Cothran <jacksoncothran@gmail.com>
2024-10-31 18:50:48 -0700

    Good progress for one session I think. Creates a window that creates a black circle when and where you left click.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,35 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main()
-{
-    auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
-    window.setFramerateLimit(144);
+int main(int argc, char* argv[]) {
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event(); window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
+    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode(1000, 1000), "sfml-test");
+    window.setVerticalSyncEnabled(true);
+
+    
+
+    while (window.isOpen()) {
+
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
+
         }
 
-        window.clear();
+
+        window.clear(sf::Color::White);
+        
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            sf::CircleShape circle(20);
+            circle.setFillColor(sf::Color::Black);
+            circle.setPosition(sf::Mouse::getPosition(window).x - circle.getRadius(), sf::Mouse::getPosition(window).y - circle.getRadius());
+            window.draw(circle);
+        }
+
         window.display();
     }
+
 }
